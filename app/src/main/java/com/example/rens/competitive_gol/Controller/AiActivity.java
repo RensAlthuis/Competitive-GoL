694aeae86cd3b0d911a5de479cc85bd69e5ddb69
Voltooid <abdulkarim_veder@live.nl>
2017-05-18 15:30:48 +0200

    created all Activity files and the Layouts are done for now(it will change later on). Next on is getting the buttons and such to work ;D!

--- app/src/main/java/com/example/rens/competitive_gol/Controller/AiActivity.java
+++ app/src/main/java/com/example/rens/competitive_gol/Controller/AiActivity.java
@@ -1,12 +1,20 @@
-package com.example.rens.competitive_gol;
+package com.example.rens.competitive_gol.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.rens.competitive_gol.R;
 
 public class AiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_ai);
     }
 }
--- app/src/main/java/com/example/rens/competitive_gol/Controller/TutorialActivity.java
+++ app/src/main/java/com/example/rens/competitive_gol/Controller/TutorialActivity.java
@@ -2,11 +2,19 @@ package com.example.rens.competitive_gol.Controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.rens.competitive_gol.R;
 
 public class TutorialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_tutorial);
     }
 }
